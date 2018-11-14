46afdcdc61b3b81709f442ddfc71e4c971306151
KWANSA <nangamu-yannick.kwansa@etu.univ-lyon1.fr>
2018-11-14 18:03:04 +0100

    Ajout pacman

---
+++ src/com/pacman/Pacman.java
@@ -0,0 +1,16 @@
+package com.pacman;
+
+public class Pacman extends Entite
+{
+
+    public Pacman(int _x, int _y)
+    {
+        super(_x,_y);
+    }
+
+    @Override
+    public void deplacer(Grille g)
+    {
+
+    }
+}
